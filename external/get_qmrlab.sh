ce914accdbe60f6625976883dff4aabb82e635db
Enrico Glerean <enrico.glerean@aalto.fi>
2020-05-19 13:24:28 +0300

    adding files

---
+++ external/get_qmrlab.sh
@@ -0,0 +1,2 @@
+wget https://github.com/qMRLab/qMRLab/archive/v2.3.0.zip
+unzip v2.3.0.zip
---
+++ run_non_interactive.m
@@ -0,0 +1,13 @@
+close all
+clear all
+
+% adding external toolboxes
+addpath(genpath('external'))
+
+% initializing qMRLab
+
+qMRLab()
+
+% calling a test function
+noddi_try()
+
