ec823c0739698e3e4172b42be6761590817c9be6
Enrico Glerean <enrico.glerean@aalto.fi>
2020-05-19 13:44:59 +0300

    deleting

--- noddy_try.m
+++
@@ -1,8 +0,0 @@
-function noddi_try()
-
-% cd /m/nbe/scratch/aging/camcan/code/Anni/;
-Model=noddi;
-data=struct;
-data.DiffusionData=double(load_nii_data('data/testdata.nii'));
-FitResults = FitData(data,Model,0);
-FitResultsSave_nii(FitResults);
